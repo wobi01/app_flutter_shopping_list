fefb715ba1e3f90a0c01c5f5eb38769a2c295935
Wojciech <wbiskup@edu.cdv.pl>
2023-10-05 19:59:51 +0200

    //1

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/widgets/grocery_list.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shopping list',
+      theme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 147, 229, 250),
+            brightness: Brightness.dark,
+            surface: const Color.fromARGB(255, 42, 51, 59)),
+        scaffoldBackgroundColor: const Color.fromARGB(250, 50, 58, 60),
+      ),
+      home: const GroceryList(),
+    );
+  }
+}
